7a0cafd7d989dd6ca2dc35b00904a9584a56791b
berat-celik <73943621+berat-celik@users.noreply.github.com>
2021-11-18 22:14:33 +0300

    PersonelTakip
    Personel Takip Programı

---
+++ ProjePersonel/PersonelTakipProgrami/PersonelTakipProgrami/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace PersonelTakipProgrami
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMaas = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnCuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(100, 35);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 1;
+            this.txtAd.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(100, 69);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyad:";
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(100, 107);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(100, 20);
+            this.txtNo.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "T.C No";
+            // 
+            // txtYas
+            // 
+            this.txtYas.Location = new System.Drawing.Point(100, 142);
+            this.txtYas.Name = "txtYas";
+            this.txtYas.Size = new System.Drawing.Size(100, 20);
+            this.txtYas.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Yaş:";
+            // 
+            // txtMaas
+            // 
+            this.txtMaas.Location = new System.Drawing.Point(100, 179);
+            this.txtMaas.Name = "txtMaas";
+            this.txtMaas.Size = new System.Drawing.Size(100, 20);
+            this.txtMaas.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Maaş:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 223);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(615, 150);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(371, 12);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(106, 39);
+            this.btnEkle.TabIndex = 11;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnCuncelle
+            // 
+            this.btnCuncelle.Location = new System.Drawing.Point(371, 110);
+            this.btnCuncelle.Name = "btnCuncelle";
+            this.btnCuncelle.Size = new System.Drawing.Size(106, 30);
+            this.btnCuncelle.TabIndex = 12;
+            this.btnCuncelle.Text = "Güncelle";
+            this.btnCuncelle.UseVisualStyleBackColor = true;
+            this.btnCuncelle.Click += new System.EventHandler(this.btnCuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(371, 63);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(106, 38);
+            this.btnSil.TabIndex = 13;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(371, 157);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(106, 29);
+            this.btnTemizle.TabIndex = 14;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(60, 9);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(18, 13);
+            this.idLabel.TabIndex = 15;
+            this.idLabel.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(100, 5);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 385);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnCuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtMaas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtYas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMaas;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnCuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox txtId;
+    }
+}
+
---
+++ ProjePersonel/PersonelTakipProgrami/PersonelTakipProgrami/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PersonelTakipProgrami
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void getPersonel()
+        {
+            baglanti = new SqlConnection("Data Source=DESKTOP-D3NIK45; Initial Catalog=Personeller; Integrated Security=True");
+            baglanti.Open();
+            data = new SqlDataAdapter("SELECT *FROM Personel", baglanti);
+            DataTable tablo = new DataTable();
+            data.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getPersonel();
+
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtAd.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtSoyad.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtNo.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtYas.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtMaas.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+
+            if (txtNo.Text.Length != 11)
+            {
+                MessageBox.Show("TC Kimlik Numarası 11 haneli olmalıdır");
+                return;
+            }
+            string sorgu = "INSERT INTO Personel(Ad,Soyad,TcNo,Yas,Maas) VALUES(@Ad,@Soyad,@TcNo,@Yas,@Maas)";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Ad",txtAd.Text);
+            komut.Parameters.AddWithValue("@Soyad", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@TcNo", txtNo.Text.Trim());
+            komut.Parameters.AddWithValue("@Yas", txtYas.Text);
+            komut.Parameters.AddWithValue("@Maas", txtMaas.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            getPersonel();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            String sorgu = "DELETE FROM Personel WHERE id=@Id";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Id",txtId.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            getPersonel();
+        }
+
+        private void btnCuncelle_Click(object sender, EventArgs e)
+        {
+            if (txtNo.Text.Length != 11)
+            {
+                MessageBox.Show("TC Kimlik Numarası 11 haneli olmalıdır");
+                return;
+            }
+            string sorgu = "UPDATE Personel SET Ad=@Ad,Soyad=@Soyad,TcNo=@TcNo,Yas=@Yas,Maas=@Maas WHERE Ad=@Ad";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Ad", txtAd.Text);
+            komut.Parameters.AddWithValue("@Soyad", txtSoyad.Text);
+            komut.Parameters.AddWithValue("@TcNo", txtNo.Text);
+            komut.Parameters.AddWithValue("@Yas", Convert.ToInt32(txtYas.Text));
+            komut.Parameters.AddWithValue("@Maas", Convert.ToInt32(txtMaas.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            getPersonel();
+
+
+
+
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txtAd.Clear();
+            txtSoyad.Clear();
+            txtNo.Clear();
+            txtYas.Clear();
+            txtMaas.Clear();
+        }
+    } 
+}
+
